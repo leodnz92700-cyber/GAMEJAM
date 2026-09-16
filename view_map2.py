c0fd48ee3265f241bb84d4f7dacb053bb7b04804
Théo Séré <104022394+theo-sere@users.noreply.github.com>
2026-09-16 14:34:08 +0200

    Ajoute scripts debug pour analyse de carte
    Ajoute plusieurs scripts utilitaires ponctuels pour inspecter la carte TMX: visualisation ASCII des tuiles walkables, zooms locaux, verification de coordonnees/rotation d'objets Tiled et dump complet dans map_full.txt. Ces outils facilitent le diagnostic de placements/colisions (ex. Door2) sans toucher a la logique du jeu.

---
+++ view_map2.py
@@ -0,0 +1,15 @@
+﻿from src.mechanics.map_loader import load_map
+m = load_map('map.tmx')
+print('  ' + ''.join(str(i%10) for i in range(16)))
+for row in range(39, 24, -1):
+    line = ''
+    for col in range(16):
+        if not m.walkable[row][col]:
+            line += '#'
+        elif row == 34 and col == 7:
+            line += 'M'
+        elif row == 31 and col == 5:
+            line += '2'
+        else:
+            line += '.'
+    print(f'{row:2d}{line}')
---
+++ view_map_zoom2.py
@@ -0,0 +1,5 @@
+﻿from src.mechanics.map_loader import load_map
+m = load_map('map.tmx')
+print('walkable[31][5]:', m.walkable[31][5])
+print('walkable[31][4]:', m.walkable[31][4])
+print('walkable[31][6]:', m.walkable[31][6])
