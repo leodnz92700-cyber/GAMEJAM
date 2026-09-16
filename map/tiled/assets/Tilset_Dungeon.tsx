f8f2b784511ceca9be559f1ca9961ada8cd06444
Tom Moguljak <moguljak.tom@gmail.com>
2026-09-16 10:51:31 +0200

    First version of the map

---
+++ map/tiled/assets/Tilset_Dungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Tilset_Dungeon" tilewidth="32" tileheight="32" tilecount="108" columns="12">
+ <image source="../../dungeonsAndPixels/Tilesets/Tileset_Dungeon.png" width="384" height="288"/>
+</tileset>
