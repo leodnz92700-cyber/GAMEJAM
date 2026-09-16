ec3f6083d42742cbdc0f84459144e5f834372a32
Théo Séré <104022394+theo-sere@users.noreply.github.com>
2026-09-16 13:27:34 +0200

    feat: integrate final map from mapCreation

---
+++ map/tiled/assets/Objects_Overlay.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Objects_Overlay" tilewidth="32" tileheight="32" tilecount="42" columns="7">
+ <image source="../../dungeonsAndPixels/Tilesets/Objects_Overlay.png" width="224" height="192"/>
+</tileset>
